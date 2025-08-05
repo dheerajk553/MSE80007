5148d4f95bcb1f406a9624cf1215b7f95c1adb7d
Dheeraj Kumar <270647844@yoobeestudent.ac.nz>
2025-08-05 13:31:36 +1200

    week1
    wee1

---
+++ HELLO.PY
@@ -0,0 +1,13 @@
+a = 33
+b = 200
+d = 345
+c = a+b
+if b > a:
+  print (b)
+  print("print is grFFGeater")
+if a > b:
+  print(a)
+  print("a is bigger")
+elif d > b:
+  print(d)
+  print("d is bigger")
---
+++ fact.py
@@ -0,0 +1,21 @@
+def fact():
+    number = input("Enter the Numaric Number")
+    a = int(number)
+
+    if a < 0:
+        return "number is lower than zero please enter greater than zero"
+
+    elif 0 <= a <= 1:
+        return 1
+
+    else:
+        result = 1
+        for i in range(1, a + 1):
+            print(", ",i)    
+            result = result *i
+        return result
+    
+
+if __name__ == "__main__":
+    ab = fact()
+    print(" result:", ab)
