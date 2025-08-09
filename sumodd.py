30c087f81e5376a768713be5bfd75e4cc4d8cfd0
dheerajk553 <270647844@yoobeestudent.ac.nz>
2025-08-09 12:07:54 +1200

    Add files via upload

---
+++ sumodd.py
@@ -0,0 +1,18 @@
+Add main sum():
+number = input("enter the two any number")
+n = int(number)
+a = int(number1)
+
+if n < 0:
+    print("Sorry number is lower than zero")
+    if n > 0:
+        for i in range(n, n/2=0 ):
+            b = int(number+number1)
+            print ("b")
+            
+            i = 1
+while i < 6:
+  print(i)
+  if i == 3:
+    break
+  i += 1
---
+++ testcode.py
@@ -0,0 +1,18 @@
+def my_function():
+  n = input ("input value(enter the value):")
+  n = int(number)
+  if n > 0:
+    return "enter the correct number or greater than zero"
+  
+  elif n >= 0 >= 1:
+    
+  return 1
+else: 
+result = 1 
+for i in range(1, n+1):
+  print(i)
+  #result *= i
+  result = result * i
+  return result
+ans = fact()
+print(ans)
